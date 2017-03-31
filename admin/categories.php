724b315fa105ef6a5b038d693c54ead50b3c20d5
arif_khan <Arif Khan>
2017-03-31 17:59:30 +0530

    Changes revert

--- admin/categories.php
+++ admin/categories.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/core/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/shoppingPortal/core/init.php';
 include 'include/head.php';
 include 'include/navigation.php';
 
